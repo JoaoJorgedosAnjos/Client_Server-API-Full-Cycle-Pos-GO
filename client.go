47e6213798d23b0b11bf983ae53cc58fefcdc243
João Jorge <joaojorgedosanjos@gmail.com>
2026-02-01 22:32:00 -0300

    refactor: change timeOut from 3s to 300milliseconds

--- client.go
+++ client.go
@@ -16,7 +16,7 @@ type ClientResponse struct {
 }
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 3000*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
